1c275f78937a51fd2232f0ae36886bae599a320c
Julian Benegas <julianbenegas99@gmail.com>
2024-04-09 12:49:03 -0300

    export fragmentOnRecursiveCollection

--- cli/src/render/client/renderClient.ts
+++ cli/src/render/client/renderClient.ts
@@ -40,7 +40,7 @@ ${renderClientTypesImports({ mutationType, queryType, subscriptionType })}
   export { GenqlError }
 
   import types from './types'
-  export { fragmentOn, QueryGenqlSelection, QueryResult } from './schema'
+  export { fragmentOn, fragmentOnRecursiveCollection, QueryGenqlSelection, QueryResult } from './schema'
   const typeMap = linkTypeMap(types as any)
 
   ${renderClientType({ mutationType, queryType, subscriptionType })}
